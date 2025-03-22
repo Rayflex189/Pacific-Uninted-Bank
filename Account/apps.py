a065a8ca74ecbe9c2d04ded460f9283509c1a52d
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-03-22 03:31:31 +0100

    Update apps.py
    Save

--- Account/apps.py
+++ Account/apps.py
@@ -1,6 +1,19 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+import os
 
-
-class AccountConfig(AppConfig):
+class AxisAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Account'
+
+    def ready(self):
+        User = get_user_model()
+
+        # Load superuser credentials from environment variables
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "adminpassword")
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(username=username, email=email, password=password)
+            print(f"Superuser '{username}' created successfully")
